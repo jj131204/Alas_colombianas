d2fbe4c8ef1359809f11b999650a2b183e823188
adrian-blip <58608388+adrian-blip@users.noreply.github.com>
2021-10-27 14:48:59 -0500

    Add files via upload

---
+++ habitad.sql
@@ -0,0 +1,41 @@
+INSERT INTO "habitats" ("ha_name", "ha_description")
+VALUES
+    (
+        "Estuarios",
+        "No esa aun"
+    ),
+    VALUES
+    (
+        "Playas",
+        "No esa aun"
+    ),
+    VALUES
+    (
+        "Manglares",
+        "No esa aun"
+    ),
+    VALUES
+    (
+        "Bosque seco",
+        "No esa aun"
+    ),
+    VALUES
+    (
+        "Cuerpos de agua dulce",
+        "No esa aun"
+    ),
+    VALUES
+    (
+        "Pastizales",
+        "No esa aun"
+    ),
+    VALUES
+    (
+        "Matorrales",
+        "No esa aun"
+    ),
+    VALUES
+    (
+        "Zonas urbanas",
+        "No esa aun"    
+    );
---
+++ status.sql
@@ -0,0 +1,8 @@
+INSERT INTO "estatus" ("es_name",)
+VALUES
+    (
+        "Amenazado"
+    ),
+    (
+        "No amenazado"
+    );
