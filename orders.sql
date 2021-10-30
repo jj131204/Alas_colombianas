619a40f4d8798a0650ba78270e1531d91bf87fbf
adrian-blip <58608388+adrian-blip@users.noreply.github.com>
2021-10-30 18:05:22 -0500

    Update orders.sql

--- orders.sql
+++ orders.sql
@@ -1,120 +1,118 @@
-INSERT INTO "orders" (or_name, "or_description")
+INSERT INTO orders (or_name, or_description)
 VALUES
     (
         ' Tinamiformes Huxley',
         'los Tinamiformes comprenden una sola familia (Tinamidae) con dos subfamilias distintas, que contiene 46 especies que se encuentran en México, América Central y América del Sur. La palabra "tinamou" proviene del término galibi para estas aves, tinamu. Tinamous se ha considerado tradicionalmente como el grupo hermano de las ratites no voladoras, pero un trabajo reciente las coloca bien dentro de la radiación de las ratites, lo que implica que las ratites basales podrían volar. '
-    ,1
     ),
     (
         'Anseriformes',
         'Los Anseriformes son un orden de aves neognatas que comprende 162 especies1​ repartidas en tres familias: los Anhimidae (con 3 especies), Anseranatidae (con una sola especie), y los Anatidae (con 156 o más especies de ánades o patos; ocas, gansos o ánsares; cisnes; yaguasas; serretas; eideres; barnaclas; cauquenes; porrones; y tarros). El grupo fue bautizado por el nombre latino de la oca «Anser» (la misma etimología que el término español ánsar), y por ello el nombre de este orden significa «los que tienen forma de oca».'
-    ,2
+   
     ),
     (
         'Galliformes',
         'Los Galliformes son un clado de aves neognatas con categoría taxonómica de orden (comúnmente conocidas como gallináceas) que cuenta con 283 especies.1​El grupo fue bautizado por el nombre latino del gallo «Gallus», y por ello el nombre de este orden significa «los que tienen forma de gallo». Incluye las gallinas, perdices, pavos, pintadas y chachalacas, entre otros. Son principalmente aves terrestres, de picos y patas fuertes. Suelen ser malos voladores, limitándose su vuelo a planeos cortos y de escasa elevación. Habitan en la mayor parte de los continentes, a excepción de las regiones más áridas y los hielos perpetuos.'
-    ,3
+   
     ),
     (
         'Podicipediforme',
         'Los podicipédidos (Podicipedidae) son la única familia actual del orden Podicipediformes. Son aves acuáticas cosmopolitas (se encuentran en todos los continentes) conocidas vulgarmente con el nombre de somormujos, macáes, zampullines o zambullidores.
-
 Son aves acuáticas que a menudo se zambullen en busca de alimento o como conducta evasiva, de ahí su nombre común de zambullidor. Son muy buenos buceadores, bastantes gregarios, vuelan poco, necesitan largos carreteos para levantar vuelo, cuando caminan (raramente) en tierra, andan erguidos. Construyen nidos flotantes, ponen varios huevos generalmente blancuzcos, que luego toman color. Son parecidos a los patos. '
-    ,4
+  
     ),
     (
         'Pelecaniformes',
         'Los pelecaniformes son un orden de aves neognatas de hábitos acuáticos, medianas y grandes, de distribución mundial. El grupo fue bautizado por el nombre latino del pelícano «Pelecanus», y por ello el nombre de este orden significa «los que tienen forma de pelícano». Tienen patas con cuatro dedos del pie palmeados (membrana interdigital). La mayoría tiene un parche en la garganta de piel desnuda (parche gular). Se alimentan principalmente de peces, calamares, especies marinas en general. Anidan en colonias, son aves monógamas, los polluelos son nidícolas generalmente.'
-    ,5
+    
     ),
     (
         'Suliformes',
         'Los Suliformes constituye un orden de aves entre las que se encuentran los populares cormoranes, fragatas o los alcatraces, tradicionalmente estaban comprendidas con anterioridad en el orden Pelecaniformes, del que guardan cierta relación por haber evolucionado de un ancestro común, pero con ciertos rasgos distintivos según estudios relacionados con su genética y morfología.
         El orden Suliformes se distribuye en 4 familias: Anhingidae, Fregatidae, Phalacrocoracidae y Sulidae. En total algunos taxomistas reconocen 8 géneros y unas 56 especies.'
-    ,6
+  
     ),
     (
         'Ciconiiformes',
         'Los Ciconiiformes son un orden de aves neognatas cuya amplitud y composición ha sido muy discutida durante años. Los últimos avances taxonómicos sugieren que solo debería incluir la familia Ciconiidae. Se conocen vulgarmente como cigüeñas. Son aves de cuello largo, zancudas y acuáticas que habitan en las regiones templadas y cálidas del mundo. Su dieta consiste en ranas, peces y pequeñas aves o mamíferos. Se distribuyen principalmente en Europa, Asia y África, con solo tres especies en América.'
-    ,7
+   
     ),
     (
         'Cathartiformes',
         'Los Cathartiformes son un orden de aves rapaces diurnas, compuesto por 2 familias, una extinta y la otra aún con representantes vivientes, cuyos integrantes son denominados buitres del Nuevo Mundo las cuales incluyen siete especies que se encuentran distribuidas por casi toda América. Se alimentan de carroña, a veces de vegetales y de pequeños animales vivos.'
-    ,8
+   
     ),
     (
         'Phoenicopteriformes',
         'Los Phoenicopteriformes son un orden de aves acuáticas neognatas que incluye a los actuales flamencos y sus parientes extintos. El grupo fue bautizado por el nombre griego del flamenco «Phoenicopterus», por ello el nombre de este orden significa «los que tienen forma de flamenco». Este orden evolucionó a partir de aves nadadoras.'
-    ,9
+    
     ),
     (
         'Accipitriformes',
         'El orden Accipitriformes incluye la mayoría de las rapaces diurnas. Este orden no existe en todas las clasificaciones. Tradicionalmente, todas las rapaces diurnas (incluidos los buitres del Nuevo Mundo) estaban situadas en el orden Falconiformes. En la clasificación de Sibley-Ahlquist, quedó desmontado este orden, al incluir las familias Accipitridae y Sagittariidae al orden Ciconiiformes, mientras que los buitres del Nuevo Mundo pasaban a formar parte de la familia Ciconiidae.'
-    ,10
+   
     ),
     (
         'Falconiformes',
         'Los Falconiformes son un orden​ de aves neognatas que agrupa 309 especies1​ de aves de presa conocidas como rapaces diurnas. Estas aves tienen un cuerpo fuerte y compacto, miembros robustos y cabeza voluminosa, más o menos redondeada, con un pico fuerte y cortante en forma de garfio; en la base del mismo y rodeando las aberturas nasales, existe un engrosamiento carnoso característico, llamado cera por su aspecto, que suele ser del mismo color que las patas.'
-    ,11
+    
     ),
     (
         'Gruiformes Bonaparte',
         'Los gruiformes son un orden de aves neognatas, muy diverso, que contiene actualmente nueve familias, entre las que destacan las grullas y los rascones. Tradicionalmente, las familias de aves terrestres que no parecían pertenecer a cualquier otro orden eran clasificadas como Gruiformes. Se incluyeron las avutardas de tierra, las grullas grandes, las gallinetas y gallaretas relativamente pequeñas, así como una variedad de familias muy pequeñas, algunos de ellos que contienen simplemente una sola especie. Los Gruiformes parecen tener poco en común entre sí porque son morfológicamente muy diversos. Sin embargo, la evidencia anatómica indica que varios grupos dentro de los gruiformes tradicionales aparezcan relacionados genéticamente'
-    ,12
+    
     ),
     (
         'Charadriiformes',
         'Las caradriformes (Charadriiformes) son un gran clado de aves neognatas dividido en varios grupos (seis subórdenes) que presentan 351 especies​ conocidas, distribuidas por todo el mundo en una amplia variedad de hábitats. Todas están relacionadas (de una u otra forma) con el agua, tanto dulce como salada.'
-    ,13
+    
     ),
     (
         'Columbiformes',
         'Los Columbiformes son un orden de aves neognatas que incluye a las palomas, tórtolas y formas afines, pero también al grupo de los extintos dodo (Raphus cucullatus) y solitario de Rodrigues (Pezophaps solitaria). El orden Columbiformes contiene 322 especies vivas y 12 extintas en tiempos históricos'
-    ,14
+    
     ),
     (
         'Psittaciformes',
         'Los psitaciformes (Psittaciformes)2​ son un orden de aves que incluye aproximadamente 86 géneros con 372 especies que se encuentran principalmente en las zonas tropicales y subtropicales. se subdivide en tres superfamilias: Psittacoidea (los loros típicos o papagayos), Cacatuoidea (las cacatúas) y Strigopoidea (los loros de Nueva Zelanda).Todos los miembros del orden tienen como características comunes: un pico robusto y curvado hacia abajo, la postura erguida de su cuerpo y sus garras prensiles zigodáctilas, es decir, que tienen dos dedos dirigidos hacia delante y dos hacia atrás. La mayoría de los psitaciformes son de colores intensos. Muchas especies de loros son predominantemente verdes, y otras tienen plumaje multicolor.'
-    ,15
+    
     ),
     (
         'Cuculiformes',
         'Los cuculiformes son un orden de aves neognatas que incluyen los cucos. Incluye un total de 165 especies. se los considera emparentadas con los Psittaciformes (loros, guacamayos, etc.); igual que estos, presentan dos dedos dirigidos adelante y dos dirigidos atrás, pero se diferencian en su musculatura, ya que sus capacidades prensiles son menores. Otra característica distintiva es el pico, porque la mandíbula superior no es móvil y la forma no es ganchuda. La larga cola de estas aves es, también, de plumaje menos denso que en sus parientes cercanos.'
-    ,16
+    
     ),
     (
         'Strigiformes',
         ' son un orden de aves compuesto por la familias Tytonidae (lechuzas), y la familia Strigidae que incluye búhos, mochuelos, tecolotes, autillos, cárabos, el ñacurutú y el chuncho, entre otras.  Son grandes cazadores, de hábitos generalmente nocturnos y solitarios. Se alimentan principalmente de pequeños mamíferos, insectos y otras aves, aunque algunas especies se especializan en la pesca. '
-    ,17
+    
     ),
     (
         'Caprimulgiformes',
         'Los Caprimulgiformes son un orden de aves neognatas que agrupa los podargos, los chotacabras y otras especies afines; se extienden por casi todo el planeta, menos por las regiones de clima ártico y en las islas de Nueva Zelanda. Se conocen 122 especies. Se trata de aves nocturnas con gran facilidad para camuflarse durante el día; incluso verlos en una fotografía es a veces complicado. Su plumaje es muy parecido en color y trazo a las hojas secas o a la corteza de un árbol, fenómeno conocido como cripsis. Se cree que los miembros de este orden están lejanamente emparentados con las rapaces nocturnas (orden Strigiformes) y, algunos autores los clasifican en dicho orden'
-    ,18
+    
     ),
     (
         'Apodiformes',
         'son un orden de aves neognatas caracterizadas por el pequeño tamaño de las patas, lo que da nombre al orden. Se conocen 438 especies. Tradicionalmente, el orden contuvo a tres familias: los vencejos y salanganas (familia Apodidae), los vencejos arborícolas (familia Hemiprocnidae) y los colibríes o picaflores (Trochilidae).'
-    ,19
+    
     ),
     (
         'Coraciiformes',
         'Los Coraciiformes son un orden de aves neognatas, normalmente muy coloridas, entre las que se encuentran los martines pescadores, los abejarucos, las carracas y los momotos. Generalmente tienen sindactilia, es decir, tres dedos del pie apuntando adelante. Incluye 218 especies.'
-    ,20
+    
     ),
     (
         'Piciformes',
         'Las piciformes son un orden de aves neognatas formado por siete familias, en las que se incluyen los pájaros carpinteros (Picidae), los tucanes (Ramphastidae) y otras aves semejantes. En estas familias hay aproximadamente unos 67 géneros y unas 339 especies. En general, las piciformes son insectívoras, aunque los tucanes se alimentan principalmente de frutas, mientras que los indicadores comen cera de abeja. La mayoría son zigodáctilas (tienen dos dedos adelante y dos atrás), lo que les sirve de gran ayuda al pasar bastante tiempo en las ramas de los árboles.'
-    ,21
+    
     ),
     (
         'Galbuliformes',
         'Los Galbuliformes son un orden de aves compuesto por 2 familias, Contienen unas 54 especies. En general, son aves tranquilas y silenciosas, que suelen permanecer quietas durante largos periodos de tiempo, esto ha dificultado el estudio de su historia natural. Los bobitos (familia Bucconidae), se caracterizan por sus colores opacos, mientras que los jacamares (familia Galbulidae) presentan tonos más vivos y hasta iridiscentes en su plumaje. '
-    ,22
+    
     ),
     (
         'Passeriformes',
         'Los paseriformes (Passeriformes) son un gran orden de aves que abarca más de la mitad de las especies de aves del mundo. Los paseriformes se conocen comúnmente como pájaros y a veces aves cantoras o pájaros cantores. Los pájaros son el grupo de vertebrados terrestres más diversificado, con más de cinco mil setecientas especies identificadas,  lo que aproximadamente duplica el número de especies del orden de mamíferos más abundante, los roedores (Rodentia). Y contiene más de ciento diez familias, ocupando el segundo puesto entre los vertebrados (tras los Perciformes).Su éxito evolutivo se debe a diversas adaptaciones al medio muy variadas y complejas, que comprenden desde su capacidad para posarse en los árboles, los usos de sus cantos, su inteligencia y la complejidad y diversidad de sus nidos.'
-    ,23
+    
     );
